247986a71b83ac6e96a2f603b15cda37ca6b9303
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2019-12-04 11:56:15 +0100

    Day 4 WIP

--- src/aoc_2019/day04.clj
+++ src/aoc_2019/day04.clj
@@ -1,26 +1,44 @@
 (ns aoc-2019.day04
-  (:require [aoc-2019.utils :refer [to-int]])
-  (:require [clojure.string :refer [split trim]]))
+  (:require
+    [aoc-2019.utils :refer [to-int]]
+    [clojure.string :refer [split trim]]))
+
+(defn- parse-input [input]
+  (map to-int (split (trim input) #"-")))
 
 (defn digits [x]
   (->> x (iterate #(quot % 10))
          (take-while pos?)
          (map #(mod % 10))))
 
-(defn has-doubles [d]
-  (let [pairs  (partition 2 (interleave (butlast d) (rest d)))]
+(defn doubles? [d]
+  (let [pairs (partition 2 (interleave (butlast d) (rest d)))]
     (->> pairs (map (partial apply =))
                (some true?)
                (boolean))))
 
+(defn strip-larger-groups [x]
+  (-> x (str)
+        (clojure.string/replace #"(\d)\1{2,}" "$1")
+        (to-int)))
+
 (defn password? [x]
   (let [d (digits x)]
-    (and (apply >= d) (has-doubles d))))
+    (and (apply >= d) (doubles? d))))
 
 (defn part-1 [input]
-  (let [[from to] (map to-int (split (trim input) #"-"))]
+  (let [[from to] (parse-input input)]
     (->> to (inc)
             (range from)
+            (map password?)
+            (filter true?)
+            (count))))
+
+(defn part-2 [input]
+  (let [[from to] (parse-input input)]
+    (->> to (inc)
+            (range from)
+            (map strip-larger-groups)
             (map password?)
             (filter true?)
             (count))))
