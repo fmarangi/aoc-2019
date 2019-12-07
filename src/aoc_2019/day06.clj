3768d25243eee097a83ab5fcd6a82fe08aa1842b
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2019-12-07 11:16:26 +0100

    Day 6

--- src/aoc_2019/day06.clj
+++ src/aoc_2019/day06.clj
@@ -5,18 +5,46 @@
 (defn- add-planet [planets [a b]]
   (assoc planets a (conj (get planets a ()) b)))
 
+(defn- add-both [planets [a b]]
+    (reduce into planets [{a (conj (get planets a #{}) b)}
+                          {b (conj (get planets b #{}) a)}]))
+
 (defn space-map [input]
   (let [rel  #(map keyword (split % #"\)"))
         lines (->> input (trim) (split-lines) (map rel))]
     (reduce add-planet {} lines)))
+
+(defn full-space-map [input]
+  (let [rel  #(map keyword (split % #"\)"))
+        lines (->> input (trim) (split-lines) (map rel))]
+    (reduce add-both {} lines)))
 
 (defn orbits [planets level curr]
   (let [sub  (get planets curr ())
         next (partial orbits planets (+ level 1))]
     (+ level (apply + (map next sub)))))
 
+(defn find-path [from to planets]
+  (loop [next [[from #{from}]]]
+    (let [[curr path] (first next) n (planets curr)]
+      (if (contains? n to)
+        path
+        (let [i #(vector % (conj path %))
+              o (map i (remove path n))
+              r (vec (rest next))]
+          (recur (reduce conj r o)))))))
+
 (defn checksum [input from]
   (orbits (space-map input) 0 from))
 
+(defn orbital-transfers [input from to]
+  (->> input (full-space-map)
+             (find-path from to)
+             (count)
+             (dec)))
+
 (defn part-1 [input]
   (checksum input :COM))
+
+(defn part-2 [input]
+  (dec( orbital-transfers input :YOU :SAN)))
