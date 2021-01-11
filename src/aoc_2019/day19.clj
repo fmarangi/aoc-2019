72aea180fc97f257b96ac194723a63a521363dba
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2021-01-11 23:25:40 +0100

    Day 2019.19

--- src/aoc_2019/day19.clj
+++ src/aoc_2019/day19.clj
@@ -1,15 +1,28 @@
 (ns aoc-2019.day19
-  (:require
-    [aoc-2019.intcode :refer [parse-program run-program]]))
+  (:require [aoc-2019.intcode :as intcode]))
 
 (defn coordinates [n]
   (fn [x] (list (quot x n) (mod x n))))
 
-(defn part-1 [input]
-  (let [beam (partial run-program (parse-program input))]
-    (->> (range (* 50 50))
-         (map (comp first beam (coordinates 50)))
-         (filter (complement zero?))
-         (count))))
+(defn beam [input]
+  (let [program (intcode/parse-program input)]
+    (comp first (partial intcode/run-program program))))
 
-(defn part-2 [input] 0)
+(defn find-100-square [input]
+  (let [stationary? (comp zero? (beam input))]
+    (loop [[x y] (repeat 500)]
+      (cond
+        (stationary? [x y])        (recur [x (inc y)])
+        (stationary? [x (+ y 99)]) (recur [(inc x) y])
+        (stationary? [(+ x 99) y]) (recur [x (inc y)])
+        :else                      [x y]))))
+
+(defn part-1 [input]
+  (->> (range (* 50 50))
+       (map (comp (beam input) (coordinates 50)))
+       (filter (complement zero?))
+       (count)))
+
+(defn part-2 [input]
+  (let [[x y] (find-100-square input)]
+    (+ (* x 10000) y)))
--- test/aoc_2019/day19_test.clj
+++ test/aoc_2019/day19_test.clj
@@ -1,9 +1,9 @@
 (ns aoc-2019.day19-test
-  (:require
-    [clojure.test :refer :all]
-    [aoc-2019.day19 :refer :all]
-    [aoc-2019.utils :refer [puzzle-input]]))
+  (:require [aoc-2019.day19 :refer :all]
+            [aoc-2019.utils :refer [puzzle-input]]
+            [clojure.test :refer :all]))
 
 (deftest solve-puzzle
   (let [input (puzzle-input "2019/day19.txt")]
-    (is (= (part-1 input) 217))))
+    (is (= (part-1 input) 217))
+    (is (= (part-2 input) 6840937))))
